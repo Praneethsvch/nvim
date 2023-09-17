97c64fd640c2c904e2a8c007f745b9d7cf6dd828
Praneethsvch <psc368@nyu.edu>
2023-09-17 12:36:40 -0400

    change colorscheme to blood sucking dracula

--- lua/p/init.lua
+++ lua/p/init.lua
@@ -2,7 +2,7 @@
 --vim.g.gruvbox_contrast_dark = 'hard'
 --vim.cmd('colorscheme nord')
 --vim.cmd('colorscheme tomorrow-night')
-vim.cmd('colorscheme monokai')
+vim.cmd('colorscheme dracula')
 require("p.set")
 require("p.remap")
 print('Hello from P')
