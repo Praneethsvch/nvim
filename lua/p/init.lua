8ed575d168450a850a701e19611b841053153b4e
Praneethsvch <psc368@nyu.edu>
2023-09-17 12:33:04 -0400

    inti

---
+++ lua/p/init.lua
@@ -0,0 +1,8 @@
+--vim.cmd('colorscheme gruvbox')
+--vim.g.gruvbox_contrast_dark = 'hard'
+--vim.cmd('colorscheme nord')
+--vim.cmd('colorscheme tomorrow-night')
+vim.cmd('colorscheme monokai')
+require("p.set")
+require("p.remap")
+print('Hello from P')
