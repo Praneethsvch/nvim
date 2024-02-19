d3521db9d9c5d496e7e9a3d81c757f9cae01d0e4
Praneethsvch <psc368@nyu.edu>
2024-02-19 18:14:42 -0500

    keymaps

--- lua/p/lazy/fugitive.lua
+++ lua/p/lazy/fugitive.lua
@@ -1,3 +1,20 @@
 return {
-	"tpope/vim-fugitive"
+	"tpope/vim-fugitive",
+
+
+	config = function()
+		
+		-- Open Git fugitive Gf 
+		vim.keymap.set("n", "<leader>Go", ":Git<CR>")
+		-- stage current file Gsc
+		vim.keymap.set("n", "<leader>Gsc", ":Gwrite<CR>")	
+		-- Git pull
+		vim.keymap.set("n", "<leader>Gf", ":Git pull<CR>")
+		-- run git command rGc 
+		--vim.keymap.set(	
+		-- git blame Gb
+		vim.keymap.set("n", "<leader>Gb", ":Git blame<CR>")
+		-- git diff Gdc 
+
+	end
 }
