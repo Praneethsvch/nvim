3633f717fe71ac922fb97642dde20ba25455afec
Praneethsvch <psc368@nyu.edu>
2024-02-19 18:15:00 -0500

    dap initial setup

--- lua/p/lazy/dap.lua
+++ lua/p/lazy/dap.lua
@@ -3,10 +3,37 @@ return {
 	
 	config = function()
 		local dap = require("dap")
-		dap.adapters.gdb = {
+		
+		-- Nordic nRF9160 adapter with JLinkDSBServerCL
+		dap.adapters.jlink = {
 			type = "executable",
-			command = "gdb",
-			args = { "-i", "dap" }
+			command = "JLinkGDBServerCL",
+			args = {"-device", "nrf9160", 
+				"-if", "swd", 
+				"-port", "2331"}
+		}
+
+		-- STM32 adapter with OpenOCD
+		dap.adapters.stlink = {
+			type = "executable",
+			command = "openocd",
+			args = {"-f", "", -- path to interface config file  
+				"-f", ""} -- path to target config file 
+		}
+
+		-- DAP C configs 
+		dap.configurations.c = {
+			{ -- for jlink and nRF9160 
+				name = "Launch JLinkGDBServer",
+				type = "jlink"}, 
+			
+			{ -- for stlink and STM32E5
+				name = "Launch OpenOCD for STM32E5",
+				type = "stlink"},
+
+			{ -- for stlink and STM32U5
+				name = "Launch OpenOCD for STM32U5",
+				type = "stlink"}
 		}
 
 	end 
