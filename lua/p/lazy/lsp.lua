cf67e003479a588bb70e9094a303fa2de3693034
Praneethsvch <psc368@nyu.edu>
2024-02-19 18:14:50 -0500

    lsp-zero

--- lua/p/lazy/lsp.lua
+++ lua/p/lazy/lsp.lua
@@ -1,5 +1,6 @@
 return {
 	"neovim/nvim-lspconfig",
+	{'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
 	dependencies = {
 		"williamboman/mason.nvim",
 		"williamboman/mason-lspconfig.nvim",
@@ -12,4 +13,30 @@ return {
 		"saadparwaiz1/cmp_luasnip",
 		"j-hui/fidget.nvim",
 	},
+
+	config = function()
+		local lsp_zero = require('lsp-zero')
+	
+		lsp_zero.preset("recommended")
+
+		lsp_zero.on_attach(function(client, bufnr)
+			-- see :help lsp-zero-keybindings
+			-- to learn the available actions
+			lsp_zero.default_keymaps({buffer = bufnr})
+		end)
+
+		-- to learn how to use mason.nvim with lsp-zero
+		-- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guide/integrate-with-mason-nvim.md
+		require('mason').setup({})
+		require('mason-lspconfig').setup({
+			ensure_installed = {
+				'clangd',
+				'rust_analyzer'
+			},
+			handlers = {
+				lsp_zero.default_setup,
+			},
+		})
+
+	end
 }
