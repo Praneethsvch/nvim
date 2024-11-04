704891f69edbe8420abb6638b94ef2375036c7ec
Praneeth Ch <praneethch@Praneeths-MBP.lan>
2024-11-03 22:42:38 -0500

    rose-pine all da way baby

--- lua/p/init.lua
+++ lua/p/init.lua
@@ -5,7 +5,7 @@ require("p.lazy_init")
 
 print('Hello from P')
 
-vim.cmd('highlight Normal guibg=black ctermbg=black')
+--vim.cmd('highlight Normal guibg=black ctermbg=black')
 vim.cmd('highlight Comment guifg=gray ctermfg=darkgray')
 vim.cmd('highlight Cursor guibg=NONE ctermbg=NONE guifg=NONE ctermfg=NONE')
 
