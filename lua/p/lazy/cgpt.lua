874dff38da0360b022a25a34602a5e2f1cb8d686
Praneeth <praneeth_ch@icloud.com>
2026-02-05 21:23:48 -0500

    remove unused AI magic

--- lua/p/lazy/cgpt.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "robitx/gp.nvim",
-    config = function()
-        local conf = {
-	    openai_api_key = { "cat", "/home/praneethch//open_api.key" },
-        }
-        require("gp").setup(conf)
-    end,
-}
